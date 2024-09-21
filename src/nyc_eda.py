bd69a7646293ba9f85113518a79f5630b5647398
hobbeskocha <ayu.yoshi.shres@gmail.com>
2024-09-20 19:25:43 -0700

    time interpolate missing data

--- src/nyc_eda.py
+++ src/nyc_eda.py
@@ -26,10 +26,10 @@ pd.options.display.max_rows = 100
 
 # ## Import Data
 
-nyc_2021 = pd.read_csv("../data/nyciso_load_act_hr_2021.csv", header = 3)
-nyc_2022 = pd.read_csv("../data/nyciso_load_act_hr_2022.csv", header = 3)
-nyc_2023 = pd.read_csv("../data/nyciso_load_act_hr_2023.csv", header = 3)
-nyc_2024 = pd.read_csv("../data/nyciso_load_act_hr_2024.csv", header = 3)
+nyc_2021 = pd.read_csv("../data/nyiso_load_act_hr_2021.csv", header = 3)
+nyc_2022 = pd.read_csv("../data/nyiso_load_act_hr_2022.csv", header = 3)
+nyc_2023 = pd.read_csv("../data/nyiso_load_act_hr_2023.csv", header = 3)
+nyc_2024 = pd.read_csv("../data/nyiso_load_act_hr_2024.csv", header = 3)
 
 # ## Merge Dataframes
 
@@ -52,71 +52,65 @@ nyc_complete.info()
 
 nyc_complete.isna().sum().sort_values()
 
-nyc_complete.columns = nyc_complete.columns.str.replace(r'[ \.\(\)\-]', '_', regex=True)
+nyc_complete.columns = nyc_complete.columns.str.replace(r'[ \(\)\-]', '_', regex=True)
 nyc_complete.columns
 
 nyc_complete["UTC_Timestamp__Interval_Ending_"] = pd.to_datetime(nyc_complete["UTC_Timestamp__Interval_Ending_"], format = "%Y-%m-%d %H:%M:%S")
 
-nyc_baltimore = nyc_complete[["UTC_Timestamp__Interval_Ending_", "Baltimore_Gas_and_Electric_Company_Actual_Load__MW_"]]
-nyc_baltimore.head()
+nyc_ny = nyc_complete[["UTC_Timestamp__Interval_Ending_", "J___New_York_City_Actual_Load__MW_"]]
+nyc_ny.head()
 
-nyc_baltimore.describe()
-
-# +
-nyc_baltimore_train = nyc_baltimore[nyc_baltimore["UTC_Timestamp__Interval_Ending_"].dt.year < 2024]
-nyc_baltimore_test = nyc_baltimore[nyc_baltimore["UTC_Timestamp__Interval_Ending_"].dt.year >= 2024]
-
-print(nyc_baltimore_train.shape, nyc_baltimore_test.shape)
-print(nyc_baltimore_train.tail(1))
-print(nyc_baltimore_test.head(1))
+nyc_ny.columns = ["UTC_Timestamp__Interval_Ending_", "New_York_City_Actual_Load__MW_"]
+nyc_ny.describe()
 
 # +
-datetime_index = pd.DataFrame(pd.date_range(start = nyc_baltimore_train["UTC_Timestamp__Interval_Ending_"].min(),
-                               end = nyc_baltimore_train["UTC_Timestamp__Interval_Ending_"].max(),
+nyc_ny_train = nyc_ny[nyc_ny["UTC_Timestamp__Interval_Ending_"].dt.year < 2024]
+nyc_ny_test = nyc_ny[nyc_ny["UTC_Timestamp__Interval_Ending_"].dt.year >= 2024]
+
+print(nyc_ny_train.shape, nyc_ny_test.shape)
+print(nyc_ny_train.tail(1))
+print(nyc_ny_test.head(1))
+
+# +
+datetime_index = pd.DataFrame(pd.date_range(start = nyc_ny_train["UTC_Timestamp__Interval_Ending_"].min(),
+                               end = nyc_ny_train["UTC_Timestamp__Interval_Ending_"].max(),
                                freq = "1h"))
 datetime_index.columns = ["UTC_Timestamp"]
 
-temp = datetime_index.merge(nyc_baltimore_train, how = "left", left_on = "UTC_Timestamp", right_on = "UTC_Timestamp__Interval_Ending_")
-temp["UTC_Timestamp__Interval_Ending_"] = pd.to_datetime(temp["UTC_Timestamp"].dt.date)
+temp = datetime_index.merge(nyc_ny_train, how = "left", left_on = "UTC_Timestamp", right_on = "UTC_Timestamp__Interval_Ending_")
 
 temp.index = temp["UTC_Timestamp"]
-temp.drop(["UTC_Timestamp"], axis = 1, inplace= True)
+temp.drop(["UTC_Timestamp", "UTC_Timestamp__Interval_Ending_"], axis = 1, inplace= True)
 
-# impute from mean of that day if other observations for that day are available
-temp["Baltimore_Gas_and_Electric_Company_Actual_Load__MW_"] = temp.groupby("UTC_Timestamp__Interval_Ending_").transform(lambda x: x.fillna(x.mean()))
+temp_interpolate = temp.interpolate(method = "time")
 
-# impute from mean of that month if there are 0 observations for a given day
-temp["UTC_Timestamp__Interval_Ending_"] = temp["UTC_Timestamp__Interval_Ending_"].dt.to_period("M")
-temp["Baltimore_Gas_and_Electric_Company_Actual_Load__MW_"] = temp.groupby("UTC_Timestamp__Interval_Ending_").transform(lambda x: x.fillna(x.mean()))
-
-print(temp.info())
-temp.head()
+print(temp_interpolate.info())
+temp_interpolate.head()
 
 # +
-nyc_baltimore_train = temp
-nyc_baltimore_train_daily = nyc_baltimore_train.drop(["UTC_Timestamp__Interval_Ending_"], axis = 1)
-nyc_baltimore_train_daily = nyc_baltimore_train_daily.resample("D").sum()
+nyc_ny_train = temp_interpolate
+nyc_ny_train_daily = nyc_ny_train.resample("D").sum()
 
-nyc_baltimore_train_daily.head()
+nyc_ny_train_daily.head()
 # -
 
 # ### Data Visualization
 
 # +
-sns.boxplot(nyc_baltimore_train_daily).set(title = "Baltimore Daily")
+sns.boxplot(nyc_ny_train_daily).set(title = "NYC Daily")
 plt.show()
 
-sns.boxplot(nyc_baltimore_train).set(title = "Baltimore Hourly")
+sns.boxplot(nyc_ny_train).set(title = "NYC Hourly")
 plt.show()
 
 # +
-sns.histplot(nyc_baltimore_train_daily).set(title = "Baltimore Daily")
+sns.histplot(nyc_ny_train_daily).set(title = "NYC Daily")
 plt.show()
 
-sns.histplot(nyc_baltimore_train).set(title = "Baltimore Daily")
+sns.histplot(nyc_ny_train).set(title = "NYC Hourly")
 plt.show()
 # -
 
-sns.lineplot(data = nyc_baltimore_train_daily, x = nyc_baltimore_train_daily.index, y = "Baltimore_Gas_and_Electric_Company_Actual_Load__MW_")
+sns.lineplot(data = nyc_ny_train_daily, x = nyc_ny_train_daily.index, y = "New_York_City_Actual_Load__MW_")
 plt.xticks(rotation = 45)
 plt.show()
