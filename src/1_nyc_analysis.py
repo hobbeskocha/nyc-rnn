b371d56cc329534c03527e163484593bf216e25e
hobbeskocha <ayu.yoshi.shres@gmail.com>
2024-10-23 18:37:57 -0700

    gru first pass

--- src/1_nyc_analysis.py
+++ src/1_nyc_analysis.py
@@ -104,8 +104,8 @@ def init_weights(m):
     if isinstance(m, nn.Linear):
         # Apply Xavier initialization to Linear layers
         nn.init.xavier_uniform_(m.weight)
-    elif isinstance(m, nn.LSTM):
-        # Apply Xavier initialization to LSTM weight matrices
+    elif isinstance(m, nn.LSTM) or isinstance(m, nn.GRU):
+        # Apply Xavier initialization to LSTM or GRU weights
         for name, param in m.named_parameters():
             if 'weight_ih' in name:
                 nn.init.xavier_uniform_(param.data)
@@ -154,12 +154,14 @@ for epoch in range(epochs):
     print(f"Epoch {epoch + 1} with loss: {loss.item()}")
     losses.append(loss.item())
 
-loss_df = pd.DataFrame({"epoch": list(range(epochs)), "loss": losses})
-sns.lineplot(loss_df, x = "epoch", y = "loss")
+lstm_loss_df = pd.DataFrame({"epoch": list(range(epochs)), "loss": losses})
+sns.lineplot(lstm_loss_df, x = "epoch", y = "loss")
 plt.xlabel("Epochs")
 plt.ylabel("MSE Loss")
-plt.title("Training Loss over 20 Epochs")
+plt.title(f"LSTM Training Loss over {epochs} Epochs")
 plt.show()
+
+print("Average LSTM Train Loss", lstm_loss_df["loss"].mean())
 
 # ### Model Inference
 
@@ -219,8 +221,8 @@ model_predictions
 
 model_predictions.isna().sum()
 
-model_rmse = root_mean_squared_error(model_predictions["Actual_Load"], model_predictions["Predicted_Load"])
-print(model_rmse)
+lstm_model_rmse = root_mean_squared_error(model_predictions["Actual_Load"], model_predictions["Predicted_Load"])
+print(lstm_model_rmse)
 
 nyc_predictions = pd.merge(nyc_test[24:], model_predictions, on = "UTC_Timestamp")
 nyc_predictions
@@ -232,6 +234,7 @@ nyc_predictions_daily = nyc_predictions.resample("D").sum()
 sns.lineplot(nyc_predictions, x = nyc_predictions.index, y = "Predicted_Load", label = "Predicted")
 sns.lineplot(nyc_predictions, x = nyc_predictions.index, y = "Actual_Load", label = "Actual", alpha = 0.7)
 plt.xticks(rotation = 45)
+plt.title("LSTM Load Demand Predictions vs Actuals")
 
 plt.savefig("../artifacts/nyc-predicted-actual-line.png")
 plt.show()
@@ -345,4 +348,168 @@ sns.lineplot(xgb_y_viz, x = xgb_y_viz.index, y = "Actual_Load_MW", label = "Actu
 plt.xticks(rotation = 45)
 
 plt.savefig("../artifacts/xgb-predicted-actual-line.png")
+plt.show()
+# -
+
+# ## GRU Modeling
+
+# +
+nyc_train = pd.read_csv("../data/nyc_ny_train_hourly_interpolated.csv", index_col= "UTC_Timestamp")
+nyc_test = pd.read_csv("../data/nyc_ny_test_hourly.csv", index_col= "UTC_Timestamp")
+
+nyc_train.columns = ["Actual_Load_MW", "Temperature_Fahrenheit"]
+nyc_test.columns = ["Actual_Load_MW", "Temperature_Fahrenheit"]
+
+
+# -
+
+# ### Class Definitions
+
+class GRUModel(nn.Module):
+    def __init__(self, input, hidden, n_layers, dropout_prob = 0.1):
+        super(GRUModel, self).__init__()
+        self.hidden_size = hidden
+        self.num_layers = n_layers
+        self.gru = nn.GRU(
+            input_size = input,
+            hidden_size = hidden,
+            num_layers = n_layers,
+            batch_first = True,
+            dropout = dropout_prob
+        )
+        self.dropout = nn.Dropout(dropout_prob)
+        self.fc = nn.Linear(hidden, 1)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        out, _ = self.gru(x, h0)
+        out = self.dropout(out[:, -1, :])
+        out = self.fc(out)
+        return out
+
+
+# ### Model Training
+
+# +
+sequence_length = 24
+batch_size = 32
+input_size = 2
+hidden_size = 50
+num_layers = 2
+dropout_probability = 0.2
+epochs = 30
+
+normalizer = MinMaxScaler(feature_range=(0, 1))
+nyc_train_normalized = nyc_train.copy()
+nyc_train_normalized = pd.DataFrame(normalizer.fit_transform(nyc_train_normalized),
+                                     columns=nyc_train.columns,
+                                     index=nyc_train.index)
+
+train_elec_dataset = ElectricLoadDataset(nyc_train_normalized, sequence_length)
+train_dataloader = DataLoader(train_elec_dataset, batch_size = batch_size, shuffle = False)
+
+gru = GRUModel(input_size, hidden_size, num_layers, dropout_probability)
+gru.apply(init_weights)
+criterion = nn.MSELoss()
+optimizer = optim.Adam(gru.parameters(), lr = 0.0001)
+losses = list()
+# -
+
+gru.train()
+for epoch in range(epochs):
+    for x, y in train_dataloader:
+        x = x.view(x.size(0), sequence_length, input_size)
+        optimizer.zero_grad()
+        output = gru(x)
+        loss = criterion(output, y.view(-1, 1))
+        loss.backward()
+        nn.utils.clip_grad_norm_(gru.parameters(), max_norm = 1.0)
+        optimizer.step()
+
+    print(f"Epoch {epoch + 1} with loss: {loss.item()}")
+    losses.append(loss.item())
+
+gru_loss_df = pd.DataFrame({"epoch": list(range(epochs)), "loss": losses})
+sns.lineplot(gru_loss_df, x = "epoch", y = "loss")
+plt.xlabel("Epochs")
+plt.ylabel("MSE Loss")
+plt.title(f"GRU Training Loss over {epochs} Epochs")
+plt.show()
+
+print("Average GRU Train Loss", gru_loss_df["loss"].mean())
+
+# ### Model Inference
+
+# +
+nyc_test_normalized = nyc_test.copy()
+nyc_test_normalized = nyc_test_normalized.ffill(axis = 0)
+nyc_test_normalized = pd.DataFrame(normalizer.transform(nyc_test_normalized),
+                                    columns=nyc_test.columns,
+                                    index=nyc_test.index)
+
+test_elec_dataset = ElectricLoadDataset(nyc_test_normalized, sequence_length)
+test_dataloader = DataLoader(test_elec_dataset, batch_size = batch_size, shuffle  = False)
+predictions, actuals = list(), list()
+mse = nn.MSELoss()
+total_mse = 0
+n_samples = 0
+
+gru.eval()
+with torch.no_grad():
+    for seq, label in test_dataloader:
+        seq = seq.view(seq.size(0), sequence_length, input_size)
+        output = gru(seq)
+
+        predictions.append(output.numpy())
+        actuals.append(label.numpy())
+        
+        loss = mse(output, label.view(-1, 1))
+        total_mse += loss.item() * seq.size(0)
+        n_samples += seq.size(0)
+
+avg_mse = total_mse / n_samples
+print("Average GRU Test MSE: ", avg_mse)
+
+predictions = np.concatenate(predictions, axis = 0)
+actuals = np.concatenate(actuals, axis = 0)
+
+# +
+data_predictions = {"predictions": pd.Series(predictions.squeeze()),
+                    "temperature": pd.Series(nyc_test_normalized["Temperature_Fahrenheit"].values[24:])}
+data_predictions = pd.DataFrame(data_predictions)
+
+data_actuals = {"actuals": pd.Series(actuals.squeeze()),
+                    "temperature": pd.Series(nyc_test_normalized["Temperature_Fahrenheit"].values[24:])}
+data_actuals = pd.DataFrame(data_actuals)
+
+data_predictions = normalizer.inverse_transform(data_predictions)
+data_actuals = normalizer.inverse_transform(data_actuals)
+
+# +
+gru_data = {"Predicted_Load": data_predictions[:, 0],
+        "Actual_Load": data_actuals[:, 0]}
+
+gru_model_predictions = pd.DataFrame(gru_data)
+gru_model_predictions.index = nyc_test[24:].index
+gru_model_predictions
+# -
+
+gru_model_predictions.isna().sum()
+
+gru_model_rmse = root_mean_squared_error(gru_model_predictions["Actual_Load"], gru_model_predictions["Predicted_Load"])
+print(gru_model_rmse)
+
+nyc_predictions = pd.merge(nyc_test[24:], gru_model_predictions, on = "UTC_Timestamp")
+nyc_predictions
+
+# +
+nyc_predictions.index = pd.to_datetime(nyc_predictions.index)
+nyc_predictions_daily = nyc_predictions.resample("D").sum()
+
+sns.lineplot(nyc_predictions, x = nyc_predictions.index, y = "Predicted_Load", label = "Predicted")
+sns.lineplot(nyc_predictions, x = nyc_predictions.index, y = "Actual_Load", label = "Actual", alpha = 0.7)
+plt.xticks(rotation = 45)
+plt.title("GRU Load Demand Predictions vs Actuals")
+
+plt.savefig("../artifacts/gru-predicted-actual-line.png")
 plt.show()
