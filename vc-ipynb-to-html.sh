a7cf1058f20d2517896443a1c22e972d65e07cec
hobbeskocha <ayu.yoshi.shres@gmail.com>
2024-11-16 00:07:17 -0800

    version control bash scripts

---
+++ vc-ipynb-to-html.sh
@@ -0,0 +1,2 @@
+jupyter nbconvert src/0_nyc_eda.ipynb --output-dir="html" --to html
+jupyter nbconvert src/1_nyc_analysis.ipynb --output-dir="html" --to html
---
+++ vc-ipynb-to-py.sh
@@ -0,0 +1,2 @@
+jupytext --to .py --output ./src/0_nyc_eda.py src/0_nyc_eda.ipynb
+jupytext --to .py --output ./src/1_nyc_analysis.py src/1_nyc_analysis.ipynb
