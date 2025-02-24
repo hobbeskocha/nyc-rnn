3c7d15433fe152b521281b385629fc96ba45f236
hobbeskocha <ayu.yoshi.shres@gmail.com>
2025-02-23 17:09:38 -0800

    file breakdown and adjusted VC scripts

--- vc-ipynb-to-html.sh
+++ vc-ipynb-to-html.sh
@@ -1,2 +1,4 @@
 jupyter nbconvert src/0_nyc_eda.ipynb --output-dir="html" --to html
-jupyter nbconvert src/1_nyc_analysis.ipynb --output-dir="html" --to html
+jupyter nbconvert src/1_traditional_model_analysis.ipynb --output-dir="html" --to html
+jupyter nbconvert src/2_lstm_analysis.ipynb --output-dir="html" --to html
+jupyter nbconvert src/3_gru_analysis.ipynb --output-dir="html" --to html
