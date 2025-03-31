a98e98a35acd2d54877811719ab68566a365ef24
hobbeskocha <ayu.yoshi.shres@gmail.com>
2025-03-31 10:53:44 -0700

    anomaly detection py

--- vc-py-to-ipynb.sh
+++ vc-py-to-ipynb.sh
@@ -2,3 +2,4 @@ jupytext --to .ipynb --output ./src/0_nyc_eda.ipynb src/0_nyc_eda.py
 jupytext --to .ipynb --output ./src/1_traditional_model_analysis.ipynb src/1_traditional_model_analysis.py
 jupytext --to .ipynb --output ./src/2_lstm_analysis.ipynb src/2_lstm_analysis.py
 jupytext --to .ipynb --output ./src/3_gru_analysis.ipynb src/3_gru_analysis.py
+jupytext --to .ipynb --output ./src/4_anomaly_detection.ipynb src/4_anomaly_detection.py
